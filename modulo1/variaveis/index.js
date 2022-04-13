47da1a6a36630b8badf72adfcc21e3bfb1dce4d2
Guilherme Brazão Falcão <guifalcaoo@gmail.com>
2022-04-12 23:54:10 -0300

    alteração index.js

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,56 @@
+//01
+/*let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)*/ 
+
+
+//02
+/*let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)*/ 
+
+
+//03
+/*let horasTrabalhadasDia = prompt("Quantas horas você trabalha por dia?")
+let ganhoPorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${ganhoPorDia/horasTrabalhadasDia} por hora`)*/
+
+
+
+//01
+/*const nome = prompt("Qual é o seu nome?")
+const idade = prompt("Qual é sua idade?")
+
+console.log("Nome:", typeof nome)
+console.log("Idade:", typeof idade)
+//Isso foi impresso para saber qual tipo da nossa variavel//
+//Tanto o nome quanto idade ficaram como strings, por conta do "prompt"
+
+console.log("Olá", nome, "você tem", idade, "anos")*/
+
+
+
+//02
+/*const metasTrabalho = prompt("Você bateu suas metas hoje? Sim/Não")
+const estaCansado = prompt("Você se cansou muito hoje? Sim/Não")
+const vaiDescansar = prompt ("Vai dormir bem para descansar? Sim/Não")
+
+console.log("Você bateu suas metas hoje?", metasTrabalho)
+console.log("Você se cansou muito hoje?", estaCansado)
+console.log("Vai dormir bem para descansar?", vaiDescansar)*/
+
+//03
+let a = 10
+let b = 25
+
+console.log("O novo valor de a é", b)
+console.log("O novo valor de b é", a)
+
