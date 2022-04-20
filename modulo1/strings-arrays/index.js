8de02462c52beeaa9073e1816e9360ab236b2b66
Guilherme Brazão Falcão <guifalcaoo@gmail.com>
2022-04-19 23:14:13 -0300

    alteracoes nos arquivos novos

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,73 @@
+//Exercicio de interpretação de codigo
+
+//Exercicio 1
+/*let array
+console.log('a. undefined', array)
+
+array = null
+console.log('b. null', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. 11', array.length)
+
+let i = 0
+console.log('d. 3', array[i])
+
+array[i+1] = 19
+console.log('e. 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13', array)
+
+const valor = array[i+6]
+console.log('f. 9', valor)
+
+//Exercicio 2
+SUBI NUM ONIBUS EM MIRROCOS 27*/
+
+
+
+//Exercicio de escrita de codigo
+
+//Exercicio 1
+/*const nome = prompt('Qual seu nome?')
+const email = prompt('Qual seu email?')
+
+console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`)*/
+
+
+//Exercicio 2
+/*let comidasFavGui = ["pão de queijo", "pudim", "açaí", "lasanha", "macarrão"]
+console.log(comidasFavGui) //a
+
+console.log("Essas são as minhas comidas preferidas:", comidasFavGui) //b
+
+let comidaUsuario = prompt('Qual a sua comida favorita?')
+comidasFavGui.splice(1, 1)
+comidasFavGui.push(comidaUsuario)
+
+console.log(comidasFavGui) //c*/
+
+
+//Exercicio 3
+/*let listaDeTarefas = []
+let tarefa1 = prompt('Me diga uma tarefa que você precisa fazer hoje!')
+let tarefa2 = prompt('Agora me fale uma segunda tarefa!')
+let tarefa3 = prompt('E pra finalizar, uma terceira tarefa!')
+
+listaDeTarefas = [tarefa1, tarefa2, tarefa3]
+
+console.log(listaDeTarefas)
+
+let tarefaFeita = prompt('Qual das três tarefas você realizou primeiro? 1, 2 ou 3')
+
+listaDeTarefas.splice(tarefaFeita)
+
+console.log(listaDeTarefas)*/
+
+
+
+//DESAFIO
+
+//Exercicio 1
+/*const frase = prompt('Me diga 5 cores!')
+const listaArray = [frase]
+
+console.log(listaArray) //não entendi como fazer uma frase unica retornar uma a uma em array :')
