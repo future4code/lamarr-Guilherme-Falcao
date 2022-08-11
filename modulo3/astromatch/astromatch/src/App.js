b5b0a6f8366cd9fb4a8d3bba30f650a623e5cbbe
Guilherme Brazão Falcão <guifalcaoo@gmail.com>
2022-08-11 15:59:48 -0300

    arrumando branches

---
+++ modulo3/astromatch/astromatch/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Homepage } from "./components/Home/Homepage";
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+      {/* socorro deus */}
+    </div>
+  );
+}
+export default App;
