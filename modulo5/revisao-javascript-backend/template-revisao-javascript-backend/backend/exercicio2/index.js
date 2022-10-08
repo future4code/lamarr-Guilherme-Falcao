a33d1bc86dd9b4080e7771108f1fc45f1e68d678
Guilherme Brazão Falcão <guifalcaoo@gmail.com>
2022-10-07 21:09:17 -0300

    finalizando revisao, incompleto

--- modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio2/index.js
@@ -1,25 +1,30 @@
-console.log("exercício 2");
+console.log("Revisão de Javascript - Pré Backend - exercício 2");
 
-let clients =[
-	{ id: 1, nome: "Fulano" },
+// Cadastro de cliente
+let clients =
+[
+    { id: 1, nome: "Fulano" },
 	{ id: 2, nome: "Ciclano" },
 	{ id: 3, nome: "Beltrano" },
-	{ id: 4, nome: "Fulana" }
+	{ id: 4, nome: "Fulana" },
 ];
+newClients = { id: 5, nome: "Gui" };
 
-newClients = {id: 4, nome: "Gui"};
-
-if (clients.id === newClients.id) (console.log(`Erro. Parâmetro inválido, o "id" já existe`));
- else if(clients.push(newClients))
-console.log(clients);
-
+if (clients.id === newClients.id)(console.log(`Erro. Parâmetro inválido, o "id" já existe`));
+else if (clients.push(newClients));
+(console.log(clients))
 
 
 
-let valorTabuada = 7
 
-    if (valorTabuada === String(valorTabuada)) console.log(`Erro. Parâmetro inválido (deve ser digitado um número)`)
-    else if (valorTabuada > 10) console.log(`Erro. Parâmetro inválido (número precisa valer entre 1 e 10)`);
-    else {for (let y = 1; y < 11; y++) {
-        console.log(`${valorTabuada} x ${y} = ${valorTabuada * y}`);
-    }};
+
+
+ // Geração de tabuada
+// let valorTabuada = 3
+// console.log(`Tabuada do ${valorTabuada}:`);
+
+//     if (valorTabuada === String(valorTabuada)) console.log(`Erro. Parâmetro inválido (deve ser digitado um número)`)
+//     else if (valorTabuada > 10) console.log(`Erro. Parâmetro inválido (número precisa valer entre 1 e 10)`);
+//     else {for (let y = 1; y < 11; y++) {
+//         console.log(`${valorTabuada} x ${y} = ${valorTabuada * y}`);
+//     }};
--- modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio4/exercicio.js
+++ modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio4/exercicio.js
@@ -1,1 +1,27 @@
-console.log("exercicio 4");
+console.log("Revisão de Javascript - Pré Backend - exercicio 4");
+
+const primeiraLista = [
+	{
+		nome: "Banana"
+	},
+	{
+		nome: "Laranja"
+	}
+]
+const segundaLista= [
+	{
+		nome: "Laranja"
+	},
+	{
+		nome: "Cebola"
+	}
+]
+const arrayConcatenado = primeiraLista.concat(segundaLista);
+const geraItensUnicos = new Map();
+
+arrayConcatenado.forEach((alimentos) =>{
+    if (!geraItensUnicos.has(alimentos.nome)){
+        geraItensUnicos.set(alimentos.nome, alimentos);
+    }
+});
+console.log([...geraItensUnicos.values()]);
