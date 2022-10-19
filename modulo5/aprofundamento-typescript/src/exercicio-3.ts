3d6e5d8716660713883260c54c33543f763a00b2
Guilherme Brazão Falcão <guifalcaoo@gmail.com>
2022-10-18 21:08:25 -0300

    exercicios aprofundamento em typescript

---
+++ modulo5/aprofundamento-typescript/src/exercicio-3.ts
@@ -0,0 +1,37 @@
+// Exercicio 3
+type Post = { 
+    autor:string,
+    texto:string,
+}
+
+const posts: Array<Post> = [
+    {
+      autor: "Alvo Dumbledore",
+      texto: "Não vale a pena viver sonhando e se esquecer de viver"
+    },
+    {
+      autor: "Severo Snape",
+      texto: "Menos 10 pontos para Grifinória!"
+    },
+    {
+      autor: "Hermione Granger",
+      texto: "É levi-ô-sa, não levio-sá!"
+    },
+    {
+      autor: "Dobby",
+      texto: "Dobby é um elfo livre!"
+    },
+    {
+      autor: "Lord Voldemort",
+      texto: "Avada Kedavra!"
+    }
+  ]
+
+  function buscarPostsPorAutor(posts:Post[], autorInformado:string) {
+    return posts.filter(
+        (post) => {
+        return post.autor === autorInformado
+      }
+    )
+  }
+  console.table(buscarPostsPorAutor(posts, "Lord Voldemort"));
